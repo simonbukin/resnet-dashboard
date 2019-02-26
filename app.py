cf24511da35cd2a600bd119a5874f1f034552acd
simonbcodes <simonbukin@gmail.com>
2019-02-26 14:58:26 -0800

    main update loop works with a test API

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from flask import Flask, render_template, request, jsonify
+import requests
+import random
 
 app = Flask(__name__)
-
-inc = 0
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
 @app.route('/_loop')
 def loop():
-    global inc
-    inc += 1
-    return jsonify(result=inc)
+    test_json = 'https://jsonplaceholder.typicode.com/users'
+    r = requests.get(test_json)
+    rand_users = random.sample(r.json(), random.choice(range(1,10)))
+    return jsonify(rand_users)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,9 +1,12 @@
-var timer = setInterval(timing_loop, 200);
+var timer = setInterval(timing_loop, 1000);
 
 function timing_loop() {
   $.getJSON($SCRIPT_ROOT + '/_loop', {
     // for arg values
   }, function(data) {
-    $("#result").text(data.result);
+    $("#test").empty()
+    for(var i = 0; i < data.length; i++) {
+      $("#test").append("<li>" + data[i].name + "</li>");
+    }
   });
 }
