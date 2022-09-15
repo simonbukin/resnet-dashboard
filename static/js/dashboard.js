9786b8073f9ee9afa813f733d3534291a3ef46e1
Simon Bukin <simonbukin@gmail.com>
2022-09-15 16:14:17 -0700

    Removed Tasks section

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -5,45 +5,22 @@ $(document).ready(function() {
 
   // when calendar event sent
   socket.on('calendar', function(msg) {
-    var elem = $('#housecall');
-    elem.empty(); // get and empty housecall div
-    if(msg > 0) { // at least 1 housecall
-      elem.append( // add notification for X housecall(s)
-      "<div class='notification is-danger'>" +
-        "<h3 class='title is-3'>There " + ((msg == 1) ? "is 1" : "are " + msg) + " housecall" + ((msg > 1) ? "s today" : " today </h3>") +
-      "</div>");
-    } else { // otherwise add message of the day
-      elem.append("<p class='is-size-3'>Remember to clock in!</p>");
-    }
+    ;
   });
 
   // update water day display
   socket.on('water', function(msg) {
-    var elem = $('#water');
-    elem.empty();
-    if (msg === 1) {
-      elem.append(
-        "<div class='notification is-info'>" + 
-          "<h3 class='title is-3'>It is water day</h3>" + 
-        "</div>"
-      );
-    }
+    ;
   })
 
   // when sheet event sent
   socket.on('sheets', function(msg) {
     // console.log("sheets", msg);
-    $("#tasks").empty(); // empty tasks div
-    for(var i = 0; i < msg.length; i++) { // add each task title as a table row
-      $("#tasks").append("<tr><td class='is-size-3'>" + msg[i]['title'] + "</td></tr>");
-    }
+    ;
   });
 
   socket.on('trello', function(msg) {
-    $("#tasks").empty(); // empty tasks div
-    for(var i = 0; i < msg.length; i++) { // add each task title as a table row
-      $('#tasks').append("<tr><td class='is-size-3'>" + msg[i] + "</td></tr>");
-    }
+    ;
   });
 
   // when itr event sent
